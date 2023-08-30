6b43dcff170c6a539a9dcc8c8de47412ba90b49d
Hitesh Aggarwal <hitesh33231@gmail.com>
2023-08-30 10:20:36 +0530

    remove efm-configs-nvim

--- .config/nvim/lua/user/plugins/lsp/init.lua
+++ .config/nvim/lua/user/plugins/lsp/init.lua
@@ -6,47 +6,21 @@ return {
       "williamboman/mason.nvim",
       "folke/neodev.nvim",
       "hrsh7th/cmp-nvim-lsp",
-      { "creativenull/efmls-configs-nvim", version = "v1.x.x" },
     },
     config = function()
       require("user.plugins.lsp.diagnostics")
       local mason = require("mason-lspconfig")
       require("mason").setup()
       require("neodev").setup()
-      local stylua = require("efmls-configs.formatters.stylua")
-      local prettier_d = {
-        formatCommand = 'prettierd "${INPUT}"',
-        formatStdin = true,
-      }
-      local black = require("efmls-configs.formatters.black")
-      local languages = {
-        lua = { stylua },
-        html = { prettier_d },
-        css = { prettier_d },
-        javascript = { prettier_d },
-        typescript = { prettier_d },
-        python = { black },
-      }
-
-      local efmls_config = {
-        filetypes = vim.tbl_keys(languages),
-        settings = {
-          rootMarkers = { ".git/" },
-          languages = languages,
-        },
-        init_options = {
-          documentFormatting = true,
-          documentRangeFormatting = true,
-        },
-      }
       mason.setup({
         ensure_installed = { "lua_ls" },
       })
+      local capabilities = require("cmp_nvim_lsp").default_capabilities(vim.lsp.protocol.make_client_capabilities())
       mason.setup_handlers({
         function(server_name) -- default handler (optional)
           require("lspconfig")[server_name].setup({
-            capabilities = require("cmp_nvim_lsp").default_capabilities(vim.lsp.protocol.make_client_capabilities()),
             single_file_support = true,
+            capabilities = capabilities,
           })
         end,
         -- Next, you can provide targeted overrides for specific servers.
@@ -55,11 +29,10 @@ return {
         -- 	require("rust-tools").setup({})
         -- end,
         ["efm"] = function()
-          require("lspconfig").efm.setup(vim.tbl_extend("force", efmls_config, {
-            -- Pass your custom lsp config below like on_attach and capabilities
-            --
-            capabilities = require("cmp_nvim_lsp").default_capabilities(vim.lsp.protocol.make_client_capabilities()),
-          }))
+          require("lspconfig").efm.setup({
+            init_options = { documentFormatting = true },
+            capabilities = capabilities,
+          })
         end,
       })
     end,
