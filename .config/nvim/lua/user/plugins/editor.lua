baa202cc0731c4c8dc01782b84d69b3972cdb1f4
Hitesh Aggarwal <hitesh33231@gmail.com>
2023-10-03 23:37:01 +0530

    fix

--- .config/nvim/lua/user/plugins/editor.lua
+++ .config/nvim/lua/user/plugins/editor.lua
@@ -6,12 +6,8 @@ return {
   },
   {
     "lukas-reineke/indent-blankline.nvim",
-    config = function()
-      require("indent_blankline").setup({
-        show_current_context = true,
-        show_current_context_start = true,
-      })
-    end,
+    main = "ibl",
+    config = true,
   },
   {
     "utilyre/barbecue.nvim",
