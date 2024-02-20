11361b3b7bfa598f8d0f228cb8b28e81f12c9f8e
Hitesh Aggarwal <hitesh33231@gmail.com>
2024-02-20 22:03:35 +0530

    Don't use mason-lspconfig, configure individual servers instead

--- .config/nvim/lua/user/plugins/lsp/init.lua
+++ .config/nvim/lua/user/plugins/lsp/init.lua
@@ -1,35 +1,43 @@
 return {
-  {
-    "williamboman/mason-lspconfig.nvim",
-    dependencies = {
-      "neovim/nvim-lspconfig",
-      "williamboman/mason.nvim",
-      "folke/neodev.nvim",
-      "hrsh7th/cmp-nvim-lsp",
-    },
-    config = function()
-      require("user.plugins.lsp.diagnostics")
-      local mason = require("mason-lspconfig")
-      require("mason").setup()
-      require("neodev").setup()
-      mason.setup({
-        ensure_installed = { "lua_ls" },
-      })
-      local capabilities = require("cmp_nvim_lsp").default_capabilities(vim.lsp.protocol.make_client_capabilities())
-      mason.setup_handlers({
-        function(server_name) -- default handler (optional)
-          require("lspconfig")[server_name].setup({
-            single_file_support = true,
-            capabilities = capabilities,
-          })
-        end,
-        -- Next, you can provide targeted overrides for specific servers.
-        -- For example, a handler override for the `rust_analyzer`:
-        -- ["rust_analyzer"] = function()
-        -- 	require("rust-tools").setup({})
-        -- end,
-        ["tsserver"] = function() end,
-      })
-    end,
+  "neovim/nvim-lspconfig",
+  dependencies = {
+    "williamboman/mason.nvim",
+    "hrsh7th/cmp-nvim-lsp",
   },
+  config = function()
+    require("user.plugins.lsp.diagnostics")
+    require("mason").setup()
+    local capabilities = require("cmp_nvim_lsp").default_capabilities()
+    require("lspconfig").lua_ls.setup({
+      capabilities = capabilities,
+      on_init = function(client)
+        local path = client.workspace_folders[1].name
+        if not vim.loop.fs_stat(path .. "/.luarc.json") and not vim.loop.fs_stat(path .. "/.luarc.jsonc") then
+          client.config.settings = vim.tbl_deep_extend("force", client.config.settings, {
+            Lua = {
+              runtime = {
+                -- Tell the language server which version of Lua you're using
+                -- (most likely LuaJIT in the case of Neovim)
+                version = "LuaJIT",
+              },
+              -- Make the server aware of Neovim runtime files
+              workspace = {
+                checkThirdParty = false,
+                -- library = {
+                --   vim.env.VIMRUNTIME,
+                --   -- "${3rd}/luv/library"
+                --   -- "${3rd}/busted/library",
+                -- },
+                -- or pull in all of 'runtimepath'. NOTE: this is a lot slower
+                library = vim.api.nvim_get_runtime_file("", true),
+              },
+            },
+          })
+
+          client.notify("workspace/didChangeConfiguration", { settings = client.config.settings })
+        end
+        return true
+      end,
+    })
+  end,
 }
