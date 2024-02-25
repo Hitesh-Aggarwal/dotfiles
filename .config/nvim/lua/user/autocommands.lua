d29aa2363d04b8bd6c2a27b7b9ce20f24544d1f2
Hitesh Aggarwal <hitesh33231@gmail.com>
2024-02-25 20:38:01 +0530

    nothing new

--- .config/nvim/lua/user/autocommands.lua
+++ .config/nvim/lua/user/autocommands.lua
@@ -1,9 +1,8 @@
 -- autocommands
-
 vim.api.nvim_create_autocmd("FileType", {
   group = vim.api.nvim_create_augroup("no_newline_comment", { clear = true }),
   pattern = "*",
-  command = "setlocal formatoptions-=c formatoptions-=r formatoptions-=o",
+  command = "setlocal formatoptions-=cro",
   desc = "Disables automatic commenting on newline",
 })
 
--- .config/nvim/lua/user/plugins/conform.lua
+++ .config/nvim/lua/user/plugins/conform.lua
@@ -4,9 +4,7 @@ return {
     require("conform").setup({
       formatters_by_ft = {
         lua = { "stylua" },
-        -- Conform will run multiple formatters sequentially
         python = { "black" },
-        -- Use a sub-list to run only the first available formatter
         javascript = { "prettierd" },
         html = { "prettierd" },
         css = { "prettierd" },
