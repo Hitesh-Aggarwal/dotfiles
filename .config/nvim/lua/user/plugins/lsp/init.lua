acf65ab4dfbd87c8ea9f2564a657b2e4959c53df
Hitesh Aggarwal <hitesh33231@gmail.com>
2024-02-25 22:33:09 +0530

    add pyright

--- .config/nvim/lua/user/plugins/lsp/init.lua
+++ .config/nvim/lua/user/plugins/lsp/init.lua
@@ -8,6 +8,9 @@ return {
     require("user.plugins.lsp.diagnostics")
     require("mason").setup()
     local capabilities = require("cmp_nvim_lsp").default_capabilities()
+    require("lspconfig").pyright.setup({
+      capabilities = capabilities,
+    })
     require("lspconfig").clangd.setup({
       capabilities = capabilities,
     })
