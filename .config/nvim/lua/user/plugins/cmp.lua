b18a5234f181fb94d3a3a0d09d3a1ee6525e5362
Hitesh Aggarwal <95736269+Hitesh-Aggarwal@users.noreply.github.com>
2024-04-16 18:31:03 +0530

    use vsnip instead of luasnip

--- .config/nvim/lua/user/plugins/cmp.lua
+++ .config/nvim/lua/user/plugins/cmp.lua
@@ -1,34 +1,34 @@
 return {
   "hrsh7th/nvim-cmp",
   dependencies = {
-    "L3MON4D3/LuaSnip",
+    "hrsh7th/cmp-vsnip",
+    "hrsh7th/vim-vsnip",
     "hrsh7th/cmp-nvim-lsp",
     "hrsh7th/cmp-buffer",
     "hrsh7th/cmp-path",
     "hrsh7th/cmp-nvim-lsp-signature-help",
-    "saadparwaiz1/cmp_luasnip",
     "rafamadriz/friendly-snippets",
     "onsails/lspkind.nvim",
   },
   config = function()
     local cmp = require("cmp")
 
-    require("luasnip.loaders.from_vscode").lazy_load()
-
     local has_words_before = function()
+      unpack = unpack or table.unpack
       local line, col = unpack(vim.api.nvim_win_get_cursor(0))
       return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
     end
 
+    local feedkey = function(key, mode)
+      vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(key, true, true, true), mode, true)
+    end
+
     local lspkind = require("lspkind")
-    local luasnip = require("luasnip")
 
     cmp.setup({
       snippet = {
         -- REQUIRED - you must specify a snippet engine
-        expand = function(args)
-          require("luasnip").lsp_expand(args.body) -- For `luasnip` users.
-        end,
+        expand = function(args) vim.fn["vsnip#anonymous"](args.body) end,
       },
       formatting = {
         format = lspkind.cmp_format(),
@@ -42,22 +42,20 @@ return {
         ["<Tab>"] = cmp.mapping(function(fallback)
           if cmp.visible() then
             cmp.select_next_item()
-          elseif luasnip.expand_or_jumpable() then
-            luasnip.expand_or_jump()
+          elseif vim.fn["vsnip#available"](1) == 1 then
+            feedkey("<Plug>(vsnip-expand-or-jump)", "")
           elseif has_words_before() then
             cmp.complete()
           else
-            fallback()
+            fallback() -- The fallback function sends a already mapped key. In this case, it's probably `<Tab>`.
           end
         end, { "i", "s" }),
 
-        ["<S-Tab>"] = cmp.mapping(function(fallback)
+        ["<S-Tab>"] = cmp.mapping(function()
           if cmp.visible() then
             cmp.select_prev_item()
-          elseif luasnip.jumpable(-1) then
-            luasnip.jump(-1)
-          else
-            fallback()
+          elseif vim.fn["vsnip#jumpable"](-1) == 1 then
+            feedkey("<Plug>(vsnip-jump-prev)", "")
           end
         end, { "i", "s" }),
       }),
@@ -65,7 +63,7 @@ return {
         { name = "nvim_lsp" },
         { name = "buffer" },
         { name = "path" },
-        { name = "luasnip" }, -- For luasnip users.
+        { name = "vsnip" }, -- For vsnip users.
         { name = "nvim_lsp_signature_help" },
       }),
     })
