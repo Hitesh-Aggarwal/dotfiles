b65dab0c67d28e70857b5025cbcad87985b8c1b2
Hitesh Aggarwal <95736269+Hitesh-Aggarwal@users.noreply.github.com>
2024-05-26 15:25:17 +0530

    fix some depreciated things

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,7 +1,7 @@
 require("user.settings")
 -- Automatically install lazy
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
+if not vim.uv.fs_stat(lazypath) then
   vim.fn.system({
     "git",
     "clone",
--- .config/nvim/lua/user/plugins/cmp.lua
+++ .config/nvim/lua/user/plugins/cmp.lua
@@ -1,9 +1,10 @@
 return {
   "hrsh7th/nvim-cmp",
   dependencies = {
+    "neovim/nvim-lspconfig",
+    "hrsh7th/cmp-nvim-lsp",
     "hrsh7th/cmp-vsnip",
     "hrsh7th/vim-vsnip",
-    "hrsh7th/cmp-nvim-lsp",
     "hrsh7th/cmp-buffer",
     "hrsh7th/cmp-path",
     "hrsh7th/cmp-nvim-lsp-signature-help",
