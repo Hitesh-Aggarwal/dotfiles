73d96a0be622b292a46dca92abbcdbb88400fbb0
Hitesh Aggarwal <95736269+Hitesh-Aggarwal@users.noreply.github.com>
2025-05-18 17:12:51 +0530

    fix bug

--- .config/nvim/lua/user/plugins/cmp.lua
+++ .config/nvim/lua/user/plugins/cmp.lua
@@ -27,8 +27,6 @@ return {
         ["<Tab>"] = cmp.mapping(function(fallback)
           if cmp.visible() then
             cmp.select_next_item()
-          elseif vim.fn["vsnip#available"](1) == 1 then
-            feedkey("<Plug>(vsnip-expand-or-jump)", "")
           elseif has_words_before() then
             cmp.complete()
           else
@@ -39,8 +37,6 @@ return {
         ["<S-Tab>"] = cmp.mapping(function()
           if cmp.visible() then
             cmp.select_prev_item()
-          elseif vim.fn["vsnip#jumpable"](-1) == 1 then
-            feedkey("<Plug>(vsnip-jump-prev)", "")
           end
         end, { "i", "s" }),
       }),
