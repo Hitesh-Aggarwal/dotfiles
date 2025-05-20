a71ec62b6962feb1f811cc2514d383d02290010c
Hitesh Aggarwal <95736269+Hitesh-Aggarwal@users.noreply.github.com>
2025-05-20 19:18:57 +0530

    don't configure any lsp server by default

--- .config/nvim/lua/user/plugins/lsp/init.lua
+++ .config/nvim/lua/user/plugins/lsp/init.lua
@@ -8,7 +8,7 @@ return {
     require("user.plugins.lsp.diagnostics")
     require("mason").setup()
     local capabilities = require("cmp_nvim_lsp").default_capabilities()
-    local servers = { "basedpyright", "rust_analyzer" }
+    local servers = {}
     for _, lsp in ipairs(servers) do
       require("lspconfig")[lsp].setup({
         capabilities = capabilities,
